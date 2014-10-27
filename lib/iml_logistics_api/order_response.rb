0c00f943a1d2da54a84fc570825ea3e8f370128f
Nu-hin <nuinuhin@gmail.com>
2014-10-27 16:18:13 +0300

    Fixed typo

--- lib/iml_logistics_api/order_response.rb
+++ lib/iml_logistics_api/order_response.rb
@@ -12,7 +12,7 @@ module ImlLogisticsApi
     field :posted, use: 'O', pattern: 'n..8'
     field :order_status, use: 'O', pattern: 'n..8', tag: 'orderStatus'
     field :status_date, use: 'O', pattern: 'an10', tag: 'statusDate'
-    field :deliveryStatus, use: 'O', pattern: 'n..8', tag: 'deliveryStatus'
+    field :delivery_status, use: 'O', pattern: 'n..8', tag: 'deliveryStatus'
     field :delivered_products, use: 'O', array: true, type: ImlLogisticsApi::DeliveredProduct
     field :returned_products, use: 'O', array: true, type: ImlLogisticsApi::ReturnedProduct
     field :comment, use: 'O', pattern: 'an..250', tag: 'comment'
